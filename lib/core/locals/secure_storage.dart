78857572e37b2c54f30f21aff4dbb01fe551106c
marievasheaa <marievaheaa170@gmail.com>
2026-02-03 03:11:23 +0200

    add shared_preferences and secure_storage

---
+++ lib/core/locals/secure_storage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../constants/app_cached.dart';
+
+//@lazySingleton
+class CachedSecure {
+  final FlutterSecureStorage storage;
+
+  CachedSecure({required this.storage});
+
+  Future<void> saveToken(String value) async {
+    return await storage.write(key: AppCached.token, value: value);
+  }
+
+  Future<String?> getToken() async {
+    return await storage.read(key: AppCached.token);
+  }
+
+  Future<bool> containToken() async {
+    return await storage.containsKey(key: AppCached.token);
+  }
+
+  Future<void> deleteToken() async {
+    return await storage.delete(key: AppCached.token);
+  }
+}
