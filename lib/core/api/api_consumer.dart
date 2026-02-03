cdb088f9f59970a45507f000bb7127ee48484d83
marievasheaa <marievaheaa170@gmail.com>
2026-02-03 03:16:45 +0200

    add api main files

---
+++ lib/core/api/api_consumer.dart
@@ -0,0 +1,22 @@
+abstract class ApiConsumer {
+  Future<dynamic> get(
+    String endPoint, {
+    dynamic body,
+    Map<String, dynamic>? queryParameters,
+    bool formDataIsEnabled = false,
+  });
+
+  Future<dynamic> post(
+    String endPoint, {
+    dynamic body,
+    Map<String, dynamic>? queryParameters,
+    bool formDataIsEnabled = false,
+  });
+
+  Future<dynamic> delete(
+    String endPoint, {
+    dynamic body,
+    Map<String, dynamic>? queryParameters,
+    bool formDataIsEnabled = false,
+  });
+}
---
+++ lib/core/api/base_response.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'base_response.g.dart';
+
+@JsonSerializable(
+    createToJson: false, genericArgumentFactories: true, includeIfNull: true)
+class BaseResponse {
+  final int? status;
+  final dynamic data;
+  final dynamic message;
+
+  BaseResponse({
+    required this.status,
+    required this.data,
+    required this.message,
+  });
+
+  factory BaseResponse.fromJson(Map<String, dynamic> json) =>
+      _$BaseResponseFromJson(json);
+}
