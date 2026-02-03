cfba8bd4ae2641602291ba02d7290294875b5d4c
marievasheaa <marievaheaa170@gmail.com>
2026-02-03 03:22:37 +0200

    finish register params

---
+++ lib/features/auth/sign_up/data/params/register_params.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'register_params.g.dart';
+
+@JsonSerializable(createFactory: false)
+class RegisterParams {
+  @JsonKey(name: 'first_name')
+  final String firstName;
+  @JsonKey(name: 'last_name')
+  final String lastName;
+  final String email;
+  final String password;
+  @JsonKey(name: 'password_confirmation')
+  final String confirmPassword;
+
+  RegisterParams({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.confirmPassword});
+
+  Map<String, dynamic> toJson() => _$RegisterParamsToJson(this);
+}
