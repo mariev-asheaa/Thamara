f3d84783d13948d6f1a5c2c2552a4d8d48397fe5
marievasheaa <marievaheaa170@gmail.com>
2026-04-16 23:36:14 +0200

    integrate profile cubit and ui

--- lib/features/settings/presentation/views/personal_information_view.dart
+++ lib/features/settings/presentation/views/personal_information_view.dart
@@ -1,9 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:thamara/features/settings/presentation/cubit/profile_cubit.dart';
 
 import '../../../../core/color_manager/app_colors.dart';
+import '../../../../core/widgets/custom_error.dart';
 import '../../../../core/widgets/custom_header.dart';
 import '../../../../core/widgets/custome_button.dart';
 import '../../../../core/widgets/custome_labeld_field.dart';
@@ -22,10 +24,11 @@ class PersonalInformationView extends StatefulWidget {
 
 class _PersonalInformationViewState extends State<PersonalInformationView> {
   bool isEditing = false;
-  final firstNameController = TextEditingController(text: "Mohab");
-  final lastNameController = TextEditingController(text: "Mohamed");
-  final emailController = TextEditingController(text: "mohabmohamed@gmail.com");
-  final phoneController = TextEditingController(text: "1558578054");
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,92 +37,120 @@ class _PersonalInformationViewState extends State<PersonalInformationView> {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.only(top: 24.h, left: 16.w, right: 16.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
 
-              children: [
-                CustomHeader(title: LocaleKeys.personalInfo.tr()),
-                SizedBox(height: 32.h),
-                UserNameFields(
-                  firstNameController: firstNameController,
-                  secondNameController: lastNameController,
-                  enabled: isEditing,
-                  backgroundColor: isEditing
-                      ? AppColors.backgroundColor
-                      : AppColors.neutralGrey,
-                ),
-                SizedBox(height: 24.h),
-                CustomLabeledField(
-                  label: LocaleKeys.emailLabel.tr(),
-                  hintText: LocaleKeys.emailHint.tr(),
-                  controller: emailController,
-                  enabled: isEditing,
-                  backgroundColor: isEditing
-                      ? AppColors.backgroundColor
-                      : AppColors.neutralGrey,
-                ),
-                SizedBox(height: 24.h),
-
-                PhoneField(
-                  controller: phoneController,
-                  enabled: isEditing,
-                  backgroundColor: isEditing
-                      ? AppColors.backgroundColor
-                      : AppColors.neutralGrey,
-                ),
-                SizedBox(height: 24.h),
-
-                if (isEditing == false)
-                  Column(
+            child: BlocBuilder<ProfileCubit, ProfileState>(
+              builder: (context, state) {
+                final cubit = context.read<ProfileCubit>();
+                if(state is ProfileFailure){
+                  return Center(
+                    child: CustomError(
+                      error: state.errorMessage,
+                      retry: () {
+                        cubit.getProfileInfo();
+                      },
+                    ),
+                  );
+                }
+                else if(state is ProfileSuccess){
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CustomButton(
-                        text: LocaleKeys.editInfoBtn.tr(),
-                        onPressed: () {
-                          setState(() {
-                            isEditing = true;
-                          });
-                        },
+                      CustomHeader(
+                        title: LocaleKeys.personalInfo.tr(), showArrow: true,),
+                      SizedBox(height: 32.h),
+                      UserNameFields(
+                        firstNameHint:state.userModel.firstName,
+                        secondNameHint:state.userModel.secondName ,
+                        firstNameController: firstNameController,
+                        secondNameController: lastNameController,
+                        enabled: isEditing,
+                        backgroundColor: isEditing
+                            ? AppColors.backgroundColor
+                            : AppColors.neutralGrey,
                       ),
-                      SizedBox(height: 16.h),
-                      CustomButton(
-                        text: LocaleKeys.deleteAccountBtn.tr(),
-                        isPrimary: false,
-                        textColor: AppColors.criticalColor,
-                        borderColor: AppColors.criticalColor,
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) => const DeleteAccountDialog(),
-                          );
-                        },
+                      SizedBox(height: 24.h),
+                      CustomLabeledField(
+                        label: LocaleKeys.emailLabel.tr(),
+                        hintText: state.userModel.email!,
+                        controller: emailController,
+                        enabled: isEditing,
+                        backgroundColor: isEditing
+                            ? AppColors.backgroundColor
+                            : AppColors.neutralGrey,
                       ),
-                    ],
-                  )
-                else
-                  Column(
-                    children: [
-                      CustomButton(
-                        text: LocaleKeys.save.tr(),
-                        onPressed: () {
-                          setState(() {
-                            isEditing = false;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 16.h),
-                      CustomButton(
-                        text: LocaleKeys.cancel.tr(),
-                        isPrimary: false,
+                      SizedBox(height: 24.h),
 
-                        onPressed: () {
-                          setState(() {
-                            isEditing = false;
-                          });
-                        },
+                      PhoneField(
+                        phoneHint: state.userModel.phoneNumber,
+                        controller: phoneController,
+                        enabled: isEditing,
+                        backgroundColor: isEditing
+                            ? AppColors.backgroundColor
+                            : AppColors.neutralGrey,
                       ),
+                      SizedBox(height: 24.h),
+
+                      if (isEditing == false)
+                        Column(
+                          children: [
+                            CustomButton(
+                              text: LocaleKeys.editInfoBtn.tr(),
+                              onPressed: () {
+                                setState(() {
+                                  isEditing = true;
+                                });
+                              },
+                            ),
+                            SizedBox(height: 16.h),
+                            CustomButton(
+                              text: LocaleKeys.deleteAccountBtn.tr(),
+                              isPrimary: false,
+                              textColor: AppColors.criticalColor,
+                              borderColor: AppColors.criticalColor,
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (
+                                      context) => const DeleteAccountDialog(),
+                                );
+                              },
+                            ),
+                          ],
+                        )
+                      else
+                        Column(
+                          children: [
+                            CustomButton(
+                              text: LocaleKeys.save.tr(),
+                              onPressed: () {
+                                setState(() {
+                                  isEditing = false;
+                                });
+                              },
+                            ),
+                            SizedBox(height: 16.h),
+                            CustomButton(
+                              text: LocaleKeys.cancel.tr(),
+                              isPrimary: false,
+
+                              onPressed: () {
+                                setState(() {
+                                  isEditing = false;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
                     ],
-                  ),
-              ],
+                  );
+                }
+                else {
+                  return Padding(
+                    padding:  EdgeInsets.only(top: 300.h),
+                    child: Center(child: CircularProgressIndicator(color: AppColors.primaryColor,)),
+                  );
+                }
+              },
             ),
           ),
         ),
--- lib/features/settings/presentation/widgets/settings_options.dart
+++ lib/features/settings/presentation/widgets/settings_options.dart
@@ -2,10 +2,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:thamara/core/text_style_manager/text_style_manager.dart';
+import 'package:thamara/core/extentions/navigation.dart';
+import 'package:thamara/features/settings/presentation/widgets/settings_divider.dart';
 import 'package:thamara/features/settings/presentation/widgets/settings_option_item.dart';
 
 import '../../../../core/color_manager/app_colors.dart';
+import '../../../../core/routing/routes.dart';
 import '../../../../generated/locale_keys.g.dart';
 import '../views/personal_information_view.dart';
 
@@ -33,15 +35,10 @@ class SettingsOptions extends StatelessWidget {
             title: LocaleKeys.personalInfo.tr(),
             iconPath: 'assets/images/personal information.svg',
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const PersonalInformationView(),
-                ),
-              );
+              context.pushWithNamed(Routes.profileView);
             },
           ),
-
+          SettingsDivider(),
           SettingsOptionItem(
             title: LocaleKeys.languageOption.tr(),
             iconPath: 'assets/images/translate.svg',
@@ -50,13 +47,12 @@ class SettingsOptions extends StatelessWidget {
               //
             },
           ),
-
+          SettingsDivider(),
           SettingsOptionItem(
             title: LocaleKeys.themeOption.tr(),
             subtitle: "(Light Mode)",
             iconPath: 'assets/images/theme.svg',
             onTap: () {},
-            showDivider: false,
           ),
         ],
       ),
