9247c67e01372e5da93217b1d7068afca345eec9
marievasheaa <marievaheaa170@gmail.com>
2026-05-04 02:02:14 +0300

    refactor profile data source to accept edit and logout

--- lib/features/settings/data/data_source/remote_data_source/profile_info_data_source.dart
+++ lib/features/settings/data/data_source/remote_data_source/profile_info_data_source.dart
@@ -1,5 +1,9 @@
 import 'package:thamara/core/models/user_model.dart';
 
+import '../../params/edit_acc_param.dart';
+
 abstract class ProfileInfoDataSource {
   Future<UserModel> getProfileInfo();
+  Future<String> logOut();
+  Future<String> editProfile({required EditAccParam param});
 }
