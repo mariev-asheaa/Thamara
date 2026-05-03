6a1077c7806912947ff155b03fe081c896d53769
marievasheaa <marievaheaa170@gmail.com>
2026-05-03 20:00:03 +0300

    add lat long fcm token params

--- lib/features/auth/login/data/params/login_params.dart
+++ lib/features/auth/login/data/params/login_params.dart
@@ -7,9 +7,18 @@ class LoginParams {
   final String email;
   final String password;
 
+  @JsonKey(name: 'fcm_token')
+  final String? fcmToken;
+
+  final double? latitude;
+  final double? longitude;
+
   LoginParams({
     required this.email,
     required this.password,
+    this.fcmToken,
+    this.latitude,
+    this.longitude,
   });
 
   Map<String, dynamic> toJson() => _$LoginParamsToJson(this);
