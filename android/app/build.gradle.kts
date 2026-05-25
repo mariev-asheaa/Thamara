93bb19163404a2a56dd2c8dcd8f31753cf30ba8f
marievasheaa <marievaheaa170@gmail.com>
2026-05-26 01:31:12 +0300

    finish google and facebook configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
     }
 }
 dependencies {
+    implementation 'com.facebook.android:facebook-login:latest.release'
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 flutter {
