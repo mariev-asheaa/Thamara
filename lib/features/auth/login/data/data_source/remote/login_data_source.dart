c0d998f63cf412fc3f0dbb152be5f3dbdab66169
marievasheaa <marievaheaa170@gmail.com>
2026-06-09 04:49:07 +0300

    add social auth data source function

--- lib/features/auth/login/data/data_source/remote/login_data_source.dart
+++ lib/features/auth/login/data/data_source/remote/login_data_source.dart
@@ -1,8 +1,13 @@
 import '../../../../../../core/models/user_model.dart';
 import '../../params/login_params.dart';
+import '../../params/social_auth_params.dart';
 
 abstract class LoginDataSource {
   Future<UserModel> login({
     required LoginParams param,
+  });
+
+  Future<UserModel> socialLogin({
+    required SocialAuthParams param,
   });
 }
--- lib/features/auth/login/data/data_source/remote/login_data_source_implementation.dart
+++ lib/features/auth/login/data/data_source/remote/login_data_source_implementation.dart
@@ -3,6 +3,7 @@ import 'package:thamara/core/api/api_consumer.dart';
 import 'package:thamara/core/models/user_model.dart';
 
 import 'package:thamara/features/auth/login/data/params/login_params.dart';
+import 'package:thamara/features/auth/login/data/params/social_auth_params.dart';
 
 import '../../../../../../core/api/api_status_codes.dart';
 import '../../../../../../core/api/base_response.dart';
@@ -11,22 +12,34 @@ import '../../../../../../core/errors/exceptions.dart';
 import 'login_data_source.dart';
 
 @Injectable(as: LoginDataSource)
-class LoginDataSourceImplementation implements LoginDataSource{
+class LoginDataSourceImplementation implements LoginDataSource {
   final ApiConsumer apiConsumer;
 
   LoginDataSourceImplementation({required this.apiConsumer});
 
   @override
-  Future<UserModel> login({required LoginParams param}) async{
+  Future<UserModel> login({required LoginParams param}) async {
     BaseResponse response = await apiConsumer.post(
       ApiConstants.login,
       body: param.toJson(),
     );
     if (response.status == ApiStatusCodes.ok ||
         response.status == ApiStatusCodes.created) {
-      final user = UserModel.fromJson(response.data);
+      return UserModel.fromJson(response.data);
+    } else {
+      throw ServerException(response.message);
+    }
+  }
 
-      return user;
+  @override
+  Future<UserModel> socialLogin({required SocialAuthParams param}) async {
+    BaseResponse response = await apiConsumer.post(
+      ApiConstants.socialLogin,
+      body: param.toJson(),
+    );
+    if (response.status == ApiStatusCodes.ok ||
+        response.status == ApiStatusCodes.created) {
+      return UserModel.fromJson(response.data);
     } else {
       throw ServerException(response.message);
     }
