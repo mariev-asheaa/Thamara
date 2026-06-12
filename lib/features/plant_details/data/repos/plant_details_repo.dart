e5ec6f8b38b1b606f2620c98c70f85b53002b858
marievasheaa <marievaheaa170@gmail.com>
2026-06-12 04:48:36 +0300

    add track progress functions in repos

--- lib/features/plant_details/data/repos/plant_details_repo.dart
+++ lib/features/plant_details/data/repos/plant_details_repo.dart
@@ -1,9 +1,19 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../arguments/ai_comparison_arguments.dart';
 import '../models/plants_model.dart';
+import '../models/scan_record_model.dart';
+import '../params/ai_comparison_params.dart';
+import '../params/scan_params.dart';
 
 abstract class PlantDetailsRepo {
   Future<Either<Failure, List<PlantsModel>>> getAllPlants();
   Future<Either<Failure, PlantsModel>> getPlantById(int id);
-}
+  Future<Either<Failure, AiComparisonArguments>> analyzeProgress({
+    required AiComparisonParams params
+  });
+
+  Future<Either<Failure, String>> postScanResult(ScanParams scanParam,int plantId);
+  Future<Either<Failure, List<ScanRecord>>> getScanHistory(int plantId);
+}
--- lib/features/plant_details/data/repos/plant_details_repo_impl.dart
+++ lib/features/plant_details/data/repos/plant_details_repo_impl.dart
@@ -1,9 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:thamara/core/errors/failure.dart';
+import 'package:thamara/features/plant_details/data/models/scan_record_model.dart';
 import 'package:thamara/features/plant_details/data/repos/plant_details_repo.dart';
+import '../arguments/ai_comparison_arguments.dart';
 import '../data_source/remote_data_source/plant_details_data_source.dart';
 import '../models/plants_model.dart';
+import '../params/ai_comparison_params.dart';
+import '../params/scan_params.dart';
 
 @Injectable(as: PlantDetailsRepo)
 class PlantDetailsRepoImpl implements PlantDetailsRepo {
@@ -28,6 +32,36 @@ class PlantDetailsRepoImpl implements PlantDetailsRepo {
       return Right(plant);
     } catch (error) {
       print(error.toString());
+      return Left(ServerFailure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, AiComparisonArguments>> analyzeProgress({required AiComparisonParams params}) async{
+    try {
+      final model = await plantDetailsDataSource.analyzeProgress(params: params);
+      return Right(model);
+    } catch (error) {
+      return Left(ServerFailure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<ScanRecord>>> getScanHistory(int plantId)async {
+    try {
+      final model = await plantDetailsDataSource.getScanHistory(plantId);
+      return Right(model);
+    } catch (error) {
+      return Left(ServerFailure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> postScanResult(ScanParams scanParam, int plantId) async{
+    try {
+      String message = await plantDetailsDataSource.postScanResult(scanParam,plantId);
+      return Right(message);
+    } catch (error) {
       return Left(ServerFailure(error.toString()));
     }
   }
